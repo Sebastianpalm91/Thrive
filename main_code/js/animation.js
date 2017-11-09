33616a94b6a3e46ec6b558947728a0b114888cd8
Sebastian Palm <sebastian.palm91@gmail.com>
2017-11-09 20:19:40 +0100

    few more small changes and moved all folders to new

--- main_code/js/animation.js
+++ main_code/js/animation.js
@@ -113,3 +113,37 @@ buttonIcon.addEventListener('click', function() {
   backgroundElementIcon.style.backgroundColor = '#C5B085'; // byt färg på elementet
        location.href = "http://www.thrivestore.se";
 })
+
+
+var Menu = {
+
+  el: {
+    ham: $('.menu'),
+    menuTop: $('.menu-top'),
+    menuMiddle: $('.menu-middle'),
+    menuBottom: $('.menu-bottom')
+  },
+
+  init: function() {
+    Menu.bindUIactions();
+  },
+
+  bindUIactions: function() {
+    Menu.el.ham
+        .on(
+          'click',
+        function(event) {
+        Menu.activateMenu(event);
+        event.preventDefault();
+      }
+    );
+  },
+
+  activateMenu: function() {
+    Menu.el.menuTop.toggleClass('menu-top-click');
+    Menu.el.menuMiddle.toggleClass('menu-middle-click');
+    Menu.el.menuBottom.toggleClass('menu-bottom-click');
+  }
+};
+
+Menu.init();
