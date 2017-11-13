dfd5c7aaba96f46f759f19c0622289a200c9fddf
Sebastian Palm <sebastian.palm91@gmail.com>
2017-11-13 14:01:00 +0100

    last changes and done

--- main_code/js/animation.js
+++ main_code/js/animation.js
@@ -95,10 +95,6 @@ function hideAddressBar() {
         $isAnimatedThirdSingle.addClass('animated fadeIn').css('animation-delay', '1s');
       }
 
-
-
-
-
     }
 
 
@@ -125,37 +121,3 @@ buttonIcon.addEventListener('click', function() {
   backgroundElementIcon.style.backgroundColor = '#C5B085'; // byt färg på elementet
        location.href = "http://www.thrivestore.se";
 })
-
-
-var Menu = {
-
-  el: {
-    ham: $('.menu'),
-    menuTop: $('.menu-top'),
-    menuMiddle: $('.menu-middle'),
-    menuBottom: $('.menu-bottom')
-  },
-
-  init: function() {
-    Menu.bindUIactions();
-  },
-
-  bindUIactions: function() {
-    Menu.el.ham
-        .on(
-          'click',
-        function(event) {
-        Menu.activateMenu(event);
-        event.preventDefault();
-      }
-    );
-  },
-
-  activateMenu: function() {
-    Menu.el.menuTop.toggleClass('menu-top-click');
-    Menu.el.menuMiddle.toggleClass('menu-middle-click');
-    Menu.el.menuBottom.toggleClass('menu-bottom-click');
-  }
-};
-
-Menu.init();
